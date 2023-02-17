1d14a44de3d5beddc7b0badec93f302f073456ed
CivilTT <civiltt.ss@gmail.com>
2023-02-17 19:09:10 +0900

    ⚡️ Fixed card and cardsection's usage

--- src/components/Home/WorksView.vue
+++ src/components/Home/WorksView.vue
@@ -8,14 +8,18 @@ var autoplay = ref(true)
 </script>
 
 <template>
-  <div class="q-pa-md row items-start q-gutter-md">
+  <div class="q-pa-md">
     <q-card flat class="caption-card">
-      <h1>Works</h1>
-    </q-card>
-    <q-card flat class="caption-card">
-      <h1>新世界へ旅立とう。</h1>
-      <p>コマンド、テクスチャ、音楽など。</p>
-      <p>我々は様々なアプローチでちょっと新しいコンテンツをMinecraftを通して制作しています。</p>
+      <q-card-actions align="around">
+        <q-card-section>
+          <h1 class="title-h1">Works</h1>
+        </q-card-section>
+        <q-card-section>
+          <h1>新世界へ旅立とう。</h1>
+          <p>コマンド、テクスチャ、音楽など。</p>
+          <p>我々は様々なアプローチでちょっと新しいコンテンツをMinecraftを通して制作しています。</p>
+        </q-card-section>
+      </q-card-actions>
     </q-card>
   </div>
     <q-carousel
@@ -62,6 +66,14 @@ var autoplay = ref(true)
 
 <style lang="scss" scoped>
 .caption-card {
-  width: 49%
+  background-color: lightcyan;
+}
+.title-h1 {
+  width: 300px;
+  margin-bottom: 0;
+}
+
+h1 {
+  margin-top: 0;
 }
 </style>
