1b886f0455d7b5b805f668f59f1777293610e280
CivilTT <civiltt.ss@gmail.com>
2023-02-17 21:34:14 +0900

    🩹 Joint components at Home

--- src/components/Home/ContentsView.vue
+++ src/components/Home/ContentsView.vue
@@ -1,16 +1,28 @@
 <script lang="ts" setup>
-
 import { ref } from 'vue';
-
 var slide = ref(1)
 var autoplay = ref(true)
-
 </script>
 
 <template>
   <div class="q-pa-md">
     <q-card flat class="caption-card">
-      <q-card-actions align="around">
+      <q-card-actions align="around" class="title-actions">
+        <q-card-section>
+          <h1 class="title-h1">About us</h1>
+        </q-card-section>
+        <q-card-section>
+          <h1>踏み出せ、新世界へ。</h1>
+          <p>コマンド、テクスチャ、音楽など。</p>
+          <p>我々は様々なアプローチでちょっと新しいコンテンツをMinecraftを通して制作しています。</p>
+        </q-card-section>
+      </q-card-actions>
+    </q-card>
+  </div>
+
+  <div class="q-pa-md">
+    <q-card flat class="caption-card">
+      <q-card-actions align="around" class="title-actions">
         <q-card-section>
           <h1 class="title-h1">Works</h1>
         </q-card-section>
@@ -67,13 +79,23 @@ var autoplay = ref(true)
 <style lang="scss" scoped>
 .caption-card {
   background-color: lightcyan;
+  align-items: baseline;
 }
 .title-h1 {
   width: 300px;
   margin-bottom: 0;
 }
+.title-actions {
+  align-items: baseline;
+}
 
 h1 {
   margin-top: 0;
+  font-weight:lighter;
+}
+
+p {
+  margin: 0pt;
+  font-size: 16pt;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,7 @@
 <script lang="ts" setup>
 // import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import TitleView from '@/components/Home/TitleView.vue';
-import AboutView from '@/components/Home/AboutView.vue';
-import WorksView from '@/components/Home/WorksView.vue';
+import ContentsView from '@/components/Home/ContentsView.vue';
 
 </script>
 
@@ -10,8 +9,7 @@ import WorksView from '@/components/Home/WorksView.vue';
 <template>
   <div class="home">
     <TitleView />
-    <AboutView />
-    <WorksView />
+    <ContentsView />
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
