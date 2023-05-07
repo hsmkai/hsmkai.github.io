301874e5b386330ae369afaf44f20a7c843cd04d
CivilTT <civiltt.ss@gmail.com>
2023-05-07 15:36:18 +0900

    🔥 Removed old codes

--- src/components/About/HsmkaiView.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import TitleCard from '../utils/TitleCard.vue';
-</script>
-
-<template>
-  <TitleCard title="About us" sub-title=""/>
-
-  
-</template>
--- src/components/About/MembersView.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import TitleCard from '../utils/TitleCard.vue';
-</script>
-
-<template>
-  <TitleCard title="Members" sub-title="信頼する仲間達。"/>
-
-  
-</template>
