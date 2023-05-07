06a8dde117c4bde14af41b8e2e1b1cd95109b318
CivilTT <civiltt.ss@gmail.com>
2023-05-07 13:15:51 +0900

    ✨ Set the page title in browser's tab

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  pages: {
+    index: {
+      entry: "src/main.ts",
+      title: "斜向 -ハスムカイ-"
+    }
+  },
+
   transpileDependencies: [
     'quasar'
   ],
