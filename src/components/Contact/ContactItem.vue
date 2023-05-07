b100d5c1cd9c50283959041d917f902acc222c17
CivilTT <civiltt.ss@gmail.com>
2023-05-07 18:50:36 +0900

    ✨ Created the Contact page

---
+++ src/components/Contact/ContactItem.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+interface Prop {
+  title: string,
+  caption: string,
+  onClick: () => void
+}
+
+defineProps<Prop>()
+</script>
+
+<template>
+  <q-item clickable class="q-mb-lg q-ml-md contentItem" @click="onClick">
+    <q-item-section avatar>
+      <slot name="avater"/>
+    </q-item-section>
+    
+    <q-item-section style="font-size: x-large;">
+      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label caption>
+        {{ caption }}
+      </q-item-label>
+    </q-item-section>
+
+    <q-item-section side top>
+      <slot name="side"/>
+    </q-item-section>
+  </q-item>
+</template>
+
+<style scoped lang="scss">
+.contentItem {
+  width: min(500px, 95%);
+}
+</style>
--- src/components/Contact/ContactView.vue
+++ src/components/Contact/ContactView.vue
@@ -1,9 +1,50 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+import { copyToClipboard, openURL } from 'quasar';
 import TitleCard from '../utils/TitleCard.vue';
+import ContactItem from './ContactItem.vue';
+
+const copied = ref(false)
+
+function copyMailAddress() {
+  copyToClipboard('hasumukaidm@gmail.com').then(() => {copied.value = true})
+}
 </script>
 
 <template>
-  <TitleCard title="Contact" sub-title="お気軽にご連絡を。"/>
+  <TitleCard
+    title="Contact"
+    sub-title="お気軽にご連絡ください。"
+    :desc="['斜向へのご相談などがございましたら、お気軽にご連絡ください。']"
+  />
 
+  <q-list>
+    <ContactItem
+      title="Twitter DM"
+      caption="@hsmkaiへのDMを開きます（表示に少々時間がかかります）"
+      @click="openURL('https://twitter.com/messages/1121761492284493824-1428274147663122432')"
+    >
+      <template v-slot:avater>
+        <q-avatar size="2rem">
+          <q-img src="@/assets/icons/twitter_blue.svg"/>
+        </q-avatar>
+      </template>
+    </ContactItem>
 
+    <ContactItem
+      title="電子メール"
+      caption="クリックするとメールアドレスをクリップボードへコピーします"
+      @click="copyMailAddress"
+    >
+      <template v-slot:avater>
+        <q-icon size="2rem" color="primary" name="mail"/>
+      </template>
+      <template v-slot:side>
+        <q-icon v-show="copied" name="assignment_turned_in" color="green" size="1.5rem"/>
+      </template>
+    </ContactItem>
+  </q-list>
+
+  <q-img src="@/assets/Contacts/members.png" class="q-ma-lg" style="width: min(800px, 85%);"/>
+  
 </template>
