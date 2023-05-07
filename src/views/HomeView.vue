eceb647af6111d6f11be40a3c1a5e6b324a24b5b
CivilTT <civiltt.ss@gmail.com>
2023-05-07 16:33:39 +0900

    🔥 Removed demo codes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import TopImage from '@/components/Home/TitleView.vue';
 import About from '@/components/Home/AboutView.vue';
 import Member from '@/components/Home/MemberView.vue';
