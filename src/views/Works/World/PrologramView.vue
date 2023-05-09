a4a327483e667b43f40f717864389f0cd433eaeb
CivilTT <civiltt.ss@gmail.com>
2023-05-10 00:32:13 +0900

    ✨ Created Prologram's article

--- src/views/Works/World/PrologramView.vue
+++ src/views/Works/World/PrologramView.vue
@@ -4,11 +4,48 @@ import BaseArticle from '@/components/Works/baseArticle.vue';
 </script>
 
 <template>
-  <BaseArticle :imgsrc="require('@/assets/Works/Worlds/prologram.png')">
-    <TitleCard title="プログラムを構築してパズルを解け！" sub-title=""/>
+  <BaseArticle
+    product-name="prologram++"
+    :imgsrc="require('@/assets/Works/Worlds/prologram.png')"
+    download-link="https://drive.google.com/uc?id=1af7lR06kG7eW1WpDktaBGt71GvWx1CA9"
+  >
+    <TitleCard
+      title="プログラムを構築してパズルを解け！"
+      :desc="[
+        'ここはドローンが飛び回る未来世界。',
+        '君はドローン操縦士になるため、この試験飛行場へやってきた。',
+        'さぁ、君の手でプログラムを構築し、難解なステージに挑め！'
+      ]"
+    />
 
-    <p>ここはドローンが飛び回る未来世界。</p>
-    <p>君はドローン操縦士になるため、この試験飛行場へやってきた。</p>
-    <p>さぁ、君の手でプログラムを構築し、難解なステージに挑め！</p>
+    <TitleCard title="紹介映像"/>
+    <q-video src="https://www.youtube.com/embed/T-l7o9YdEq0" class="video"/>
+
+    <div class="q-pt-xl">
+      <TitleCard
+        title="プログラミング"
+        :desc="[
+          'プログラムの基本は、動作と数値。',
+          '動作ブロックと数値ブロックをセットで使うことでドローンを操作できる。',
+          '幾つかのルールがあるのでお忘れなく。',
+        ]"
+      />
+    </div>
+
+    <TitleCard
+      title="ダウンロード"
+      :desc="[
+        '右下の丸いアイコンから、規約に同意することでダウンロードできます。',
+        '対応バージョン：Minecraft 1.15.2 / 1.16 / 1.16.1'  
+      ]"
+    />
   </BaseArticle>
 </template>
+
+<style scoped lang="scss">
+.video {
+  margin: 0 auto;
+  width: min(900px, 80vw);
+  aspect-ratio: 16/9;
+}
+</style>
