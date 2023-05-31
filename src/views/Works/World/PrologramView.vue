7a0ed82eb3506bf586fc2ccd1e8c6a3026efcc07
txkodo <kodo.hsmkai@gmail.com>
2023-06-01 01:21:10 +0900

    🩹 全角スペースを半角に変更

--- src/views/Works/World/PrologramView.vue
+++ src/views/Works/World/PrologramView.vue
@@ -45,7 +45,7 @@ import DefaultCarousel from '@/components/Works/defaultCarousel.vue';
       title="ダウンロード"
       :abst="[
         '右下の丸いアイコンから、規約に同意することでダウンロードできます。',
-        'Minecraft 1.15.2 / 1.16 / 1.16.1　シングルプレイ/マルチプレイ対応'  
+        'Minecraft 1.15.2 / 1.16 / 1.16.1 シングルプレイ/マルチプレイ対応'  
       ]"
     />
   </BaseArticle>
